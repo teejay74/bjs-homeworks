39bb8a6858ae44e48008a29d75a9c866ed5585d0
Artem Makarov <banka84@ya.ru>
2020-06-23 16:51:10 +0500

    Task 2.3 fix-update

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -117,7 +117,7 @@ class Library {
   giveBookByName(bookName) {
     for (let i = 0; i < this.books.length; i++) {
       if (this.books[i].name == bookName) {
-        return console.log(this.books.splice(i, 1));
+        return this.books.splice(i, 1)[0];
       }
      
     }
